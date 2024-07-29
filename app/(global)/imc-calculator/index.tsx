0127b9397ecabdc89f50e75e8bc4e32252bbb9b0
valros76 <valerian.dufrene@gmail.com>
2024-07-29 16:39:00 +0200

    Code du 29-07-2024, 16h38

---
+++ app/(global)/imc-calculator/index.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, View } from "react-native";
+
+
+export default function ImcCalculatorScreen(){
+  return(
+    <View style={styles.view}>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent:"center",
+    alignItems:"center",
+    gap:48,
+    backgroundColor:"#F6F4E8",
+  }
+});

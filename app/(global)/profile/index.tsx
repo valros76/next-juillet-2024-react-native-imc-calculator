7fc2dce7d1eef5e32b84ae3c5d1db75c77ab3837
valros76 <valerian.dufrene@gmail.com>
2024-07-29 16:33:05 +0200

    Code du 29-07-2024, 16h33

---
+++ app/(global)/profile/index.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, View } from "react-native";
+
+
+export default function ProfileScreen(){
+  return(
+    <View style={styles.view}>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flex:1,
+    justifyContent:"flex-start",
+    alignItems:"center",
+    gap:84,
+    backgroundColor:"#F6F4E8",
+  }
+});

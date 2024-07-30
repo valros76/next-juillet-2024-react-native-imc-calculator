6b95699932b6436ee47b9e93fb9d0f4576ba44ed
valros76 <valerian.dufrene@gmail.com>
2024-07-30 11:21:01 +0200

    Code du 30-07-2024, 11h20

--- app/(global)/profile/index.tsx
+++ app/(global)/profile/index.tsx
@@ -1,10 +1,23 @@
 import { useEffect, useState } from "react";
-import { Image, StyleSheet, Text, TextInput, View } from "react-native";
-import {getSavedFirstname, modifyFirstname} from "@/shared/AsyncFunctions";
-import { CtaButton, WelcomeText } from "@/components/globals";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import {
+  getSavedFirstname,
+  modifyFirstname,
+} from "@/shared/AsyncFunctions";
+import {
+  CtaButton,
+  WelcomeText,
+} from "@/components/globals";
 
 export default function ProfileScreen() {
-
   /**
    * TODO : Régler le problème de l'avatar qui ne charge pas sur mobile !
    */
@@ -13,9 +26,8 @@ export default function ProfileScreen() {
   const [newFirstname, setNewFirstname] = useState("");
 
   useEffect(() => {
-    getSavedFirstname()
-    .then(data => {
-      if(!data){
+    getSavedFirstname().then((data) => {
+      if (!data) {
         return;
       }
       setFirstname(data);
@@ -23,73 +35,78 @@ export default function ProfileScreen() {
 
     return () => {
       setFirstname("");
-    }
+    };
   }, []);
 
   const handleModifyFirstname = async () => {
-    if(newFirstname.length <= 0){
+    if (newFirstname.length <= 0) {
       return;
     }
 
-    setNewFirstname(() => newFirstname.toLowerCase().trim());
+    setNewFirstname(() =>
+      newFirstname.toLowerCase().trim()
+    );
     setNewFirstname(() => newFirstname.replaceAll(" ", ""));
-    setNewFirstname(() => newFirstname.slice(0,1).toUpperCase() + newFirstname.slice(1, newFirstname.length));
+    setNewFirstname(
+      () =>
+        newFirstname.slice(0, 1).toUpperCase() +
+        newFirstname.slice(1, newFirstname.length)
+    );
 
-    await modifyFirstname(newFirstname).then(data => {
-      if(!data){
+    await modifyFirstname(newFirstname).then((data) => {
+      if (!data) {
         return;
       }
       setNewFirstname("");
     });
 
-    await getSavedFirstname().then(data => {
-      if(!data){
+    await getSavedFirstname().then((data) => {
+      if (!data) {
         return;
       }
       setFirstname(data);
     });
-  }
-
+  };
 
   return (
-    <View style={styles.view}>
-      <View style={styles.userInfosView}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri: "https://api.dicebear.com/8.x/bottts/svg?seed=241",
-          }}
-        />
+    <SafeAreaView style={styles.view}>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.userInfosView}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: "https://api.dicebear.com/8.x/bottts/svg?seed=241",
+            }}
+          />
 
-        <Text style={styles.firstname}>
-          {firstname}
-        </Text>
-      </View>
+          <Text style={styles.firstname}>{firstname}</Text>
+        </View>
 
-      <View style={styles.modifyUserView}>
-        <WelcomeText
-          props={{
-            text: "Un autre utilisateur ?"
-          }}
-        />
-        <TextInput
-          style={styles.firstnameInput}
-          placeholder="Nouveau prénom"
-          placeholderTextColor="#BACEC1"
-          onChangeText={setNewFirstname}
-          value={newFirstname}
-        />
-        <CtaButton
-          props={{
-            text: "Modifier",
-            actionOnPress: () => handleModifyFirstname(),
-            disabled: newFirstname.length <= 0,
-            addStyles: styles.cta,
-            addTextStyles: styles.ctaText
-          }}
-        />
-      </View>
-    </View>
+        <View style={styles.modifyUserView}>
+          <WelcomeText
+            props={{
+              text: "Un autre utilisateur ?",
+            }}
+          />
+          <TextInput
+            style={styles.firstnameInput}
+            placeholder="Nouveau prénom"
+            placeholderTextColor="#BACEC1"
+            onChangeText={setNewFirstname}
+            value={newFirstname}
+          />
+          <CtaButton
+            props={{
+              text: "Modifier",
+              actionOnPress: () => handleModifyFirstname(),
+              disabled: newFirstname.length <= 0,
+              addStyles: styles.cta,
+              addTextStyles: styles.ctaText,
+            }}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -98,25 +115,35 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-start",
     alignItems: "center",
-    gap: 84,
     backgroundColor: "#F6F4E8",
-    paddingVertical:42,
+  },
+  scrollView: {
+    width:"100%",
+    gap: 84,
+    paddingVertical: 24,
   },
   userInfosView: {
+    width:"auto",
+    justifyContent:"center",
+    alignItems:"center",
     gap: 6,
+    marginBottom: 84,
   },
   modifyUserView: {
+    width:"auto",
+    justifyContent:"center",
+    alignItems:"center",
     gap: 8,
   },
-  avatar:{
-    width:128,
-    height:128,
-    borderRadius:128,
+  avatar: {
+    width: 128,
+    height: 128,
+    borderRadius: 128,
     marginHorizontal: "auto",
   },
   firstname: {
-    fontSize:64,
-    textAlign:"center",
+    fontSize: 64,
+    textAlign: "center",
   },
   firstnameInput: {
     width: 240,
@@ -130,9 +157,9 @@ const styles = StyleSheet.create({
     borderColor: "#BACEC1",
   },
   cta: {
-    marginHorizontal:"auto",
+    marginHorizontal: "auto",
   },
-  ctaText:{
+  ctaText: {
     fontSize: 16,
-  }
+  },
 });

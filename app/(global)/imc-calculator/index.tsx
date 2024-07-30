11d209c76ca6323143ec3e38b684f60b23f17db6
valros76 <valerian.dufrene@gmail.com>
2024-07-30 14:49:47 +0200

    Code du 30-07-2024, 14h49

--- app/(global)/imc-calculator/index.tsx
+++ app/(global)/imc-calculator/index.tsx
@@ -1,25 +1,71 @@
-import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  View,
+} from "react-native";
 import { useFonts } from "expo-font";
+import { useState } from "react";
+import { CtaButton } from "@/components/globals";
 
-export default function ImcCalculatorScreen(){
-  
+export default function ImcCalculatorScreen() {
+  const [size, setSize] = useState("");
+  const [weight, setWeight] = useState("");
+
   const [loaded, error] = useFonts({
     "Titillium Web Light": require("@/assets/fonts/TitilliumWeb-Light.ttf"),
     "Titillium Web Regular": require("@/assets/fonts/TitilliumWeb-Regular.ttf"),
     "Titillium Web Bold": require("@/assets/fonts/TitilliumWeb-Bold.ttf"),
   });
-  
-  return(
+
+  return (
     <SafeAreaView style={styles.view}>
       <ScrollView style={styles.scrollView}>
         <View style={styles.contentView}>
           <View style={styles.calculateIMCFormView}>
+            <TextInput
+              placeholder="Votre taille en cm"
+              placeholderTextColor="#79797A"
+              inputMode="numeric"
+              keyboardType="numeric"
+              onChangeText={setSize}
+              value={size}
+              style={styles.textInputs}
+            />
 
+            <TextInput
+              placeholder="Votre poids en kg"
+              placeholderTextColor="#79797A"
+              inputMode="numeric"
+              keyboardType="numeric"
+              onChangeText={setWeight}
+              value={weight}
+              style={styles.textInputs}
+            />
+
+            <CtaButton
+              props={{
+                text:"Calculer mon IMC",
+                borderRadius:100,
+                addStyles: {
+                  width:"auto",
+                  marginHorizontal:"auto",
+                  paddingVertical: 10,
+                  paddingHorizontal: 24,
+                },
+                addTextStyles: {
+                  fontSize: 16,
+                  fontWeight: "bold",
+                  letterSpacing: .1,
+                }
+              }}
+            />
           </View>
 
-          <View style={styles.calculateIMCResultView}>
-
-          </View>
+          <View
+            style={styles.calculateIMCResultView}
+          ></View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -29,27 +75,39 @@ export default function ImcCalculatorScreen(){
 const styles = StyleSheet.create({
   view: {
     flex: 1,
-    backgroundColor:"#F6F4E8",
+    backgroundColor: "#F6F4E8",
   },
   scrollView: {
     width: "100%",
   },
   contentView: {
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center",
-    gap:48,
-    padding:20,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 48,
+    padding: 20,
   },
   calculateIMCFormView: {
     justifyContent: "flex-start",
-    alignItems:"stretch",
-    gap:24,
-    padding:0,
+    alignItems: "stretch",
+    gap: 24,
+    padding: 0,
   },
   calculateIMCResultView: {
-    justifyContent:"center",
-    alignItems:"center",
-    gap:12,
-  }
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 12,
+  },
+  textInputs: {
+    width: 240,
+    height: 40,
+    borderRadius: 8,
+    backgroundColor: "#ffffff",
+    textAlign: "left",
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderWidth: 1,
+    borderColor: "#BACEC1",
+    fontFamily: "Titillium Web Regular",
+  },
 });

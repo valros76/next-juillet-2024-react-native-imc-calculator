668906350266f6e7916f305f039f623bc1c9cdd7
valros76 <valerian.dufrene@gmail.com>
2024-07-31 11:12:25 +0200

    Code du 31/07/2024, 11h12

--- app/(global)/history/index.tsx
+++ app/(global)/history/index.tsx
@@ -1,22 +1,52 @@
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import { ActivityIndicator, FlatList, SafeAreaView, ScrollView, StyleSheet, Text } from "react-native";
 import { getSavedHistory } from "@/shared/AsyncFunctions";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Card } from "@/components/globals";
 
 export default function HistoryScreen(){
 
-  const debug = async () => {
+  const [datas, setDatas] = useState([]);
+
+  const debug = async (value:any = undefined) => {
+    if(value){
+      console.table(value);
+    }
     const history = await getSavedHistory();
     console.table(history);
   }
 
+  const initDatas = async () => {
+    const datas = await getSavedHistory();
+    setDatas(datas);
+  }
+
   useEffect(() => {
     debug();
+    initDatas();
   }, []);
 
   return(
     <SafeAreaView style={styles.view}>
       <ScrollView style={styles.scrollView}>
-
+        {datas.length > 0 ? (
+          <FlatList
+          data={datas}
+          renderItem={(item:any) => (<Card props={{
+            title:item.firstname
+          }}>
+            <Text>{item.date}</Text>
+            <Text>IMC : {item.imc} - {item.imcHint}</Text>
+            <Text>Taille : {item.size} {item.sizeUnit}</Text>
+            <Text>Poids : {item.weight} {item.weightUnit}</Text>
+          </Card>)}
+          keyExtractor={(item: any) => item.timestamp}
+        />
+        ) : (
+          <ActivityIndicator
+            size="large"
+            color="#E59560"
+          />
+        )}
       </ScrollView>
     </SafeAreaView>
   );

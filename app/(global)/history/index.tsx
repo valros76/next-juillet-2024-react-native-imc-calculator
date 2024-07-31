68f136a940df7e28a3511421e2552fee0cb1438d
valros76 <valerian.dufrene@gmail.com>
2024-07-31 12:06:39 +0200

    Code du 31/07/2024, 12h06

--- app/(global)/history/index.tsx
+++ app/(global)/history/index.tsx
@@ -1,70 +1,110 @@
-import { ActivityIndicator, FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { getSavedHistory } from "@/shared/AsyncFunctions";
 import { useEffect, useState } from "react";
 import { Card } from "@/components/globals";
 
-export default function HistoryScreen(){
-
+export default function HistoryScreen() {
   const [datas, setDatas] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const debug = (value:any = undefined) => {
-    if(value){
+  const debug = (value: any = undefined) => {
+    if (value) {
       console.table(value);
     }
-  }
+  };
 
   const initDatas = async () => {
     const datas = await getSavedHistory();
-    setDatas(datas);
-  }
+    setDatas(datas ?? []);
+    setLoading(false);
+  };
 
   const setCardBorderColor = (imc: number) => {
-    switch(true){
-      case (imc > 40):
+    switch (true) {
+      case imc > 40:
         return "#1D3124";
-      case (imc <= 18.5):
-      case (imc <= 40 && imc > 35):
+      case imc <= 18.5:
+      case imc <= 40 && imc > 35:
         return "#ED1C24";
-      case (imc <= 35 && imc > 30):
+      case imc <= 35 && imc > 30:
         return "#E59560";
-      case (imc <= 30 && imc > 25):
+      case imc <= 30 && imc > 25:
         return "#BACEC1";
-      case (imc <= 25 && imc > 18.5):
+      case imc <= 25 && imc > 18.5:
       default:
         return "#00A511";
     }
-  }
+  };
 
   useEffect(() => {
     initDatas();
   }, []);
 
-  return(
+  return (
     <SafeAreaView style={styles.view}>
       <ScrollView style={styles.scrollView}>
-        {datas.length > 0 ? (
+        {datas.length > 0 && !loading ? (
           <FlatList
-          data={datas}
-          renderItem={(data:any) => (<Card props={{
-            title:data.item.firstname,
-            borderColor: setCardBorderColor(Number(data.item.imc))
-          }}>
-            <Text>{data.item.date}</Text>
-            <Text>IMC : {data.item.imc} - {data.item.imcHint}</Text>
-            <Text>Taille : {data.item.size} {data.item.sizeUnit}</Text>
-            <Text>Poids : {data.item.weight} {data.item.weightUnit}</Text>
-          </Card>)}
-          keyExtractor={(item: any) => item.timestamp}
-          ItemSeparatorComponent={() => (<View style={{
-            width:"100%",
-            height:24,
-          }}></View>)}
-        />
-        ) : (
-          <ActivityIndicator
-            size="large"
-            color="#E59560"
+            data={datas}
+            renderItem={(data: any) => (
+              <Card
+                props={{
+                  title: data.item.firstname,
+                  borderColor: setCardBorderColor(
+                    Number(data.item.imc)
+                  ),
+                }}
+              >
+                <Text>{data.item.date}</Text>
+                <Text>
+                  IMC : {data.item.imc} -{" "}
+                  {data.item.imcHint}
+                </Text>
+                <Text>
+                  Taille : {data.item.size}{" "}
+                  {data.item.sizeUnit}
+                </Text>
+                <Text>
+                  Poids : {data.item.weight}{" "}
+                  {data.item.weightUnit}
+                </Text>
+              </Card>
+            )}
+            keyExtractor={(item: any) => item.timestamp}
+            ItemSeparatorComponent={() => (
+              <View
+                style={{
+                  width: "100%",
+                  height: 24,
+                }}
+              ></View>
+            )}
           />
+        ) : (
+          <>
+            {loading ? (
+              <ActivityIndicator
+                size="large"
+                color="#E59560"
+              />
+            ) : (
+              <Card
+                props={{
+                  title: "Aucunes données"
+                }}
+              >
+                <Text>Vous n'avez jamais calculé votre IMC.</Text>
+              </Card>
+            )}
+          </>
         )}
       </ScrollView>
     </SafeAreaView>
@@ -73,15 +113,15 @@ export default function HistoryScreen(){
 
 const styles = StyleSheet.create({
   view: {
-    flex:1,
-    justifyContent:"flex-start",
-    alignItems:"stretch",
-    gap:0,
-    backgroundColor:"#F6F4E8",
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    gap: 0,
+    backgroundColor: "#F6F4E8",
   },
   scrollView: {
-    backgroundColor:"transparent",
-    paddingVertical:42,
-    paddingHorizontal:20,
+    backgroundColor: "transparent",
+    paddingVertical: 42,
+    paddingHorizontal: 20,
   },
 });
